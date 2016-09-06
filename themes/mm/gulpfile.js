01ac9e5e4d5f405b06273cfef27f5d95d263f8cb
Max Milton <max@wearegenki.com>
2016-09-07 06:59:24 +1000

    Change gulp path variable scss to sass

--- themes/mm/gulpfile.js
+++ themes/mm/gulpfile.js
@@ -20,7 +20,7 @@ var paths = {
     root: '../../public',
     html: '../../public/**/*.html',
   },
-  scss: {
+  sass: {
     src: './sass/**/*.scss',
     main: './sass/main.scss',
     dest: './static/css/',
@@ -59,7 +59,7 @@ gulp.task('serve', ['css', 'js'], function() {
   });
 
   // FIXME: Run hugo AFTER sass/js to avoid having to save twice to see a change
-  gulp.watch(paths.scss.src, ['css', 'hugo']);
+  gulp.watch(paths.sass.src, ['css', 'hugo']);
   gulp.watch(paths.js.src, ['js', 'hugo']);
   gulp.watch([paths.content, paths.layouts, paths.config], ['hugo']);
   gulp.watch([paths.public.html]).on('change', browserSync.reload);
@@ -90,12 +90,12 @@ gulp.task('hugo:build', ['css:build', 'js:build'], function(fetch) {
 //----------------------------------------
 
 gulp.task('css', function() {
-  return gulp.src(paths.scss.main)
+  return gulp.src(paths.sass.main)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('app.css'))
     .pipe(sourcemaps.write(paths.maps))
-    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(gulp.dest(paths.sass.dest))
     .pipe(browserSync.stream());
 });
 
@@ -113,7 +113,7 @@ gulp.task('css:build', ['css', 'hugo', 'clean:rev'], function() {
     },
   }))
   .pipe(rev())
-  .pipe(gulp.dest(paths.scss.dest))
+  .pipe(gulp.dest(paths.sass.dest))
   .pipe(rev.manifest(paths.manifest, { merge: true }))
   .pipe(gulp.dest('.'))
 });
@@ -176,7 +176,7 @@ gulp.task('html:rev', ['html:build'], function() {
 gulp.task('clean:rev', function(){
   return del([
     // Remove old revisioned files
-    paths.scss.dest + '/app-*.css',
+    paths.sass.dest + '/app-*.css',
     paths.js.dest + '/app-*.js',
   ])
 });
