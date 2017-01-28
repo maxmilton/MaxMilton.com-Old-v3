a70b999625d3fb05c09dc986b2b1561b612cb513
Max Milton <max@wearegenki.com>
2017-01-29 07:56:16 +1100

    Refactor gulpfile to ES6 + fix linter errors

--- themes/mm/gulpfile.js
+++ themes/mm/gulpfile.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-multi-spaces */
 const browserSync = require('browser-sync').create();
 const cleanCSS    = require('gulp-clean-css');
 const del         = require('del');
@@ -15,6 +18,7 @@ const rev         = require('gulp-rev');
 const sass        = require('gulp-sass');
 const sourcemaps  = require('gulp-sourcemaps');
 const uncss       = require('gulp-uncss');
+/* eslint-enable no-multi-spaces */
 
 const paths = {
   public: {
@@ -47,7 +51,7 @@ gulp.task('clean', ['clean:public']);
 // Browser Sync
 //----------------------------------------
 
-gulp.task('serve', ['hugo', 'css', 'img'], function() {
+gulp.task('serve', ['hugo', 'css', 'img'], () => {
   browserSync.init({
     server: paths.public.root,
     reloadDelay: 200, // Give hugo a moment to generate pages
@@ -65,16 +69,16 @@ gulp.task('serve', ['hugo', 'css', 'img'], function() {
 // Hugo
 //----------------------------------------
 
-gulp.task('hugo', function(cb) {
-  exec('hugo -s ../../ --buildDrafts --baseUrl="http://localhost:3000/"', function(err, stdout, stderr) {
+gulp.task('hugo', (cb) => {
+  exec('hugo -s ../../ --buildDrafts --baseUrl="http://localhost:3000/"', (err, stdout, stderr) => {
     console.log(stdout);   // Hugo output
     console.error(stderr); // Errors
     cb(err);
   });
 });
 
-gulp.task('hugo:build', ['css:build', 'img:build'], function(cb) {
-  exec('hugo -s ../../', function(err, stdout, stderr) {
+gulp.task('hugo:build', ['css:build', 'img:build'], (cb) => {
+  exec('hugo -s ../../', (err, stdout, stderr) => {
     console.log(stdout);   // Hugo output
     console.error(stderr); // Errors
     cb(err);
@@ -85,40 +89,40 @@ gulp.task('hugo:build', ['css:build', 'img:build'], function(cb) {
 // CSS
 //----------------------------------------
 
-gulp.task('css', function() {
-  return gulp.src(paths.sass.main)
+gulp.task('css', () =>
+  gulp.src(paths.sass.main)
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write(paths.maps))
     .pipe(gulp.dest(paths.sass.dest))
-    .pipe(browserSync.stream({ match: '**/*.css' }));
-});
+    .pipe(browserSync.stream({ match: '**/*.css' }))
+);
 
-gulp.task('css:build', ['css', 'hugo', 'clean:rev'], function() {
-  return gulp.src(paths.sass.static)
-  .pipe(uncss({ html: [paths.public.html] }))
-  .pipe(postcss([
-    flexFixes(),
-    prefixer({
-      browsers: ['> 1%', 'last 2 versions'],
-      add: true,
-    }),
-  ]))
-  .pipe(cleanCSS({
-    keepSpecialComments: 0,
-  }))
-  .pipe(rev())
-  .pipe(gulp.dest(paths.sass.dest))
-  .pipe(rev.manifest(paths.manifest, { merge: true }))
-  .pipe(gulp.dest('.'));
-});
+gulp.task('css:build', ['css', 'hugo', 'clean:rev'], () =>
+  gulp.src(paths.sass.static)
+    .pipe(uncss({ html: [paths.public.html] }))
+    .pipe(postcss([
+      flexFixes(),
+      prefixer({
+        browsers: ['> 1%', 'last 2 versions'],
+        add: true,
+      }),
+    ]))
+    .pipe(cleanCSS({
+      keepSpecialComments: 0,
+    }))
+    .pipe(rev())
+    .pipe(gulp.dest(paths.sass.dest))
+    .pipe(rev.manifest(paths.manifest, { merge: true }))
+    .pipe(gulp.dest('.'))
+);
 
 //----------------------------------------
 // Images
 //----------------------------------------
 
-gulp.task('img', function() {
-  return gulp.src([paths.img.src])
+gulp.task('img', () =>
+  gulp.src([paths.img.src])
     // Resize images (use with <img> shortcode in hugo)
     .pipe(responsive({
       '*': [{
@@ -137,11 +141,11 @@ gulp.task('img', function() {
       silent: true,              // Don't spam the console
       withoutEnlargement: false, // Allow image enlargement
     }))
-    .pipe(gulp.dest(paths.img.dest));
-});
+    .pipe(gulp.dest(paths.img.dest))
+);
 
-gulp.task('img:build', ['img'], function() {
-  return gulp.src([`${paths.img.dest}/*.{jpg,png,gif,svg}`])
+gulp.task('img:build', ['img'], () =>
+  gulp.src([`${paths.img.dest}/*.{jpg,png,gif,svg}`])
     // Optimise images
     .pipe(imagemin([
       imagemin.gifsicle(),
@@ -149,15 +153,15 @@ gulp.task('img:build', ['img'], function() {
       imagemin.svgo(),
       mozjpeg(),
     ]))
-    .pipe(gulp.dest(paths.img.dest));
-});
+    .pipe(gulp.dest(paths.img.dest))
+);
 
 //----------------------------------------
 // HTML
 //----------------------------------------
 
-gulp.task('html:build', ['hugo:build'], function() {
-  return gulp.src(paths.public.html)
+gulp.task('html:build', ['hugo:build'], () =>
+  gulp.src(paths.public.html)
     .pipe(htmlmin({
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
@@ -172,30 +176,30 @@ gulp.task('html:build', ['hugo:build'], function() {
       removeOptionalTags: true,
       removeRedundantAttributes: true,
     }))
-    .pipe(gulp.dest(paths.public.root));
-});
+    .pipe(gulp.dest(paths.public.root))
+);
 
-gulp.task('html:rev', ['html:build'], function() {
+gulp.task('html:rev', ['html:build'], () => {
   const manifest = gulp.src(paths.manifest);
 
   return gulp.src(paths.public.html)
-  .pipe(replace({ manifest }))
-  .pipe(gulp.dest(paths.public.root));
+    .pipe(replace({ manifest }))
+    .pipe(gulp.dest(paths.public.root));
 });
 
 //----------------------------------------
 // Misc.
 //----------------------------------------
 
-gulp.task('clean:rev', function() {
-  return del([
+gulp.task('clean:rev', () =>
+  del([
     // Remove old revisioned files
     `${paths.sass.dest}/app-*.css`,
-  ]);
-});
+  ])
+);
 
-gulp.task('clean:build', ['html:rev'], function() {
-  return del([
+gulp.task('clean:build', ['html:rev'], () =>
+  del([
     // Remove development and non-revisioned files
     `${paths.public.root}/css/maps`,
     `${paths.public.root}/css/app.css`,
@@ -204,13 +208,13 @@ gulp.task('clean:build', ['html:rev'], function() {
     // Remove blog post drafts
     `${paths.public.root}/drafts`,
     `${paths.public.root}/drafts.html`,
-  ], { force: true });
-});
+  ], { force: true })
+);
 
-gulp.task('clean:public', function() {
-  return del([
+gulp.task('clean:public', () =>
+  del([
     // Remove entire public directory contents
     `${paths.public.root}/*`,
     `${paths.public.root}/.*`,
-  ], { force: true });
-});
+  ], { force: true })
+);
